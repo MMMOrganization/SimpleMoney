426d235198d0af87fc39202f0550d8a41ab94e35
kanghun1121 <kanghun1121@naver.com>
2024-12-11 21:38:40 +0900

    Add ScrollView

--- MMM_SideProject/MMM_SideProject/MainViewController.swift
+++ MMM_SideProject/MMM_SideProject/MainViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    let scrollView : UIScrollView = {
+        let sc = UIScrollView()
+        sc.translatesAutoresizingMaskIntoConstraints = false
+        sc.backgroundColor = .white
+        return sc
+    }()
     
+    let contentView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +30,28 @@ class MainViewController: UIViewController {
     
     func setLayout() {
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            
+            contentView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            
+            // 스크롤 가능한 높이 설정 (1300까지)
+            contentView.heightAnchor.constraint(equalToConstant: 1300),
+            
+            // 세로 방향의 스크롤뷰 설정
+            // 가로면 heightAnchor
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
     }
     
 
