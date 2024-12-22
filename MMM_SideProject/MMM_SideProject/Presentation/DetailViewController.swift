829044231ab99184f88341a6dfb1f1cd8bd540ed
kanghun1121 <kanghun1121@naver.com>
2024-12-22 16:28:53 +0900

    Feature DetailView topView Layout Complete

---
+++ MMM_SideProject/MMM_SideProject/Presentation/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  MMM_SideProject
+//
+//  Created by 강대훈 on 12/16/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ dd/dd/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  dd
+//
+//  Created by 강대훈 on 12/16/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
