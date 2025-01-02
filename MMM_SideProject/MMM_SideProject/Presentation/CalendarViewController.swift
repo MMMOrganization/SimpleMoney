2e3ea2a01ae0a37ac5b03585dacaf037cb937fd3
kanghun1121 <kanghun1121@naver.com>
2025-01-02 22:48:58 +0900

    Feature: CalendarVC Calendar 추가

---
+++ MMM_SideProject/MMM_SideProject/Presentation/CalendarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarViewController.swift
+//  MMM_SideProject
+//
+//  Created by 강대훈 on 1/1/25.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MMM_SideProject/MMM_SideProject/Presentation/DateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DateViewController.swift
+//  MMM_SideProject
+//
+//  Created by 강대훈 on 12/30/24.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
