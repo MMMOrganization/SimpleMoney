5c0f95be206a1f0ab418dd935517a7678279ab26
kanghun1121 <kanghun1121@naver.com>
2025-01-29 01:11:44 +0900

    Revert "Revert "Feat: CircleGraph MVVM-C 패턴으로 변환""
    This reverts commit eb1a9b8f247f10ff6bbf974e7ad6f0adf1e95f07.
    
    Feat: CircleGraph MVVM-Combine 패턴으로 변환"

--- MMM_SideProject/MMM_SideProject/Presentation/Graph/GraphView.swift
+++ MMM_SideProject/MMM_SideProject/Presentation/Graph/GraphView.swift
@@ -7,38 +7,33 @@
 
 import SwiftUI
 import Charts
-
-struct iPhoneOperationSystem {
-    let version: String
-    let count: Int
-    
-    static func dummyData() -> [iPhoneOperationSystem] {
-        return [
-            iPhoneOperationSystem(version: "16.0", count: 81),
-            iPhoneOperationSystem(version: "15.0", count: 13),
-            iPhoneOperationSystem(version: "14.0", count: 6)
-        ]
-    }
-}
+import Combine
 
 struct CircleGraphView: View {
     @StateObject private var viewModel: GraphViewModelForSwiftUI
     
-    init(viewModel: GraphViewModelForSwiftUI = GraphViewModelForSwiftUI()) {
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(viewModel: GraphViewModelForSwiftUI) {
         _viewModel = StateObject(wrappedValue: viewModel)
+        
+        viewModel.$selectedStyle
+            .sink { expendCount in
+                print(expendCount)
+            }.store(in: &cancellables)
     }
     
     var body: some View {
         VStack {
-            Chart(viewModel.entityData, id: \.version) { element in
+            Chart(viewModel.getEntityData(), id: \.expendType) { element in
                 SectorMark(
-                    angle: .value("Usage", element.count),
+                    angle: .value("지출 타입", element.count),
                     innerRadius: .ratio(0.618),
                     angularInset: 1.5
                 )
                 .cornerRadius(5.0)
-                .opacity((element.version == viewModel.selectedStyle?.version ?? "") ? 1 : 0.3)
-                .foregroundStyle(by: .value("Version", element.version))
+                .opacity((element.expendType.rawValue == viewModel.selectedStyle?.expendType.rawValue ?? "") ? 1 : 0.3)
+                .foregroundStyle(by: .value("expendType", element.expendType.rawValue))
             }
             .chartLegend(.hidden)
             .chartAngleSelection(value: $viewModel.selectedData)
@@ -50,10 +45,10 @@ struct CircleGraphView: View {
                     
                     VStack {
                         if let selectedStyle = viewModel.selectedStyle {
-                            Text("Usage")
+                            Text("지출 타입")
                                 .font(.callout)
                             
-                            Text("iOS \(selectedStyle.version)")
+                            Text("iOS \(selectedStyle.expendType.rawValue)")
                                 .font(.title.bold())
                             
                             Text("\(viewModel.calculatePercentage(for: selectedStyle)) %")
@@ -67,9 +62,5 @@ struct CircleGraphView: View {
             }
         }
     }
-}
-
-#Preview {
-    CircleGraphView()
 }
 
--- MMM_SideProject/MMM_SideProject/Presentation/Graph/GraphViewModel+SwiftUI.swift
+++ MMM_SideProject/MMM_SideProject/Presentation/Graph/GraphViewModel+SwiftUI.swift
@@ -7,60 +7,103 @@
 
 import SwiftUI
 import Charts
+import Combine
+import RxSwift
+import RxCocoa
+
+// Dummy 객체 (테스트용)
+enum ExpendType : String {
+    case date1 = "date1"
+    case date2 = "date2"
+    case date3 = "date3"
+}
+
+struct ExpendCount {
+    let expendType : ExpendType
+    let count : Int
+}
 
 class GraphViewModelForSwiftUI: ObservableObject {
     @Published var selectedData: Double?
-    @Published var selectedVersion: String?
+    @Published var selectedType: ExpendType?
     @Published var isSelected: Bool = false
     
-    let entityData: [iPhoneOperationSystem]
-    let cumulativeSalesRanges: [(version: String, range: Range<Double>)]
+    // MARK: - 선택된 지출 타입을 퍼블리셔해서 추적을 통해 Cell을 변경해야 함.
+    @Published var selectedStyle : ExpendCount?
     
-    init(entityData: [iPhoneOperationSystem] = iPhoneOperationSystem.dummyData()) {
-        self.entityData = entityData
+    private var calcellables = Set<AnyCancellable>()
+    
+    private var repository : DataRepositoryInterface
+    private let entityData : [ExpendCount]
+    
+    private let cumulativeSalesRanges: [(expendType: ExpendType, range: Range<Double>)]
+    
+    // TODO: - Observable로 데이터를 전달해야 함.
+    //let cumulativeSalesRanges: [(version: String, range: Range<Double>)]
+    
+    init(repository : DataRepositoryInterface) {
+        self.repository = repository
+        
+        self.entityData = [ExpendCount(expendType: .date1, count: 15),
+                           ExpendCount(expendType: .date2, count: 17),
+                           ExpendCount(expendType: .date3, count: 23)
+                          ]
         
         var cumulative = 0.0
         self.cumulativeSalesRanges = entityData.map {
             let newCumulative = cumulative + Double($0.count)
-            let result = (version: $0.version, range: cumulative..<newCumulative)
+            let result = (expendType: $0.expendType, range: cumulative..<newCumulative)
             cumulative = newCumulative
             return result
         }
+        
+        Publishers.CombineLatest3($selectedData, $selectedType, $isSelected)
+            .sink { [weak self] (selectedData, selectedType, isSelected) in
+                self?.updateSelectedStyle(selectedData: selectedData, selectedType: selectedType, isSelected: isSelected)
+            }.store(in: &calcellables)
     }
     
-    // MARK: - 선택된 그래프 비율을 통해 확인
-    var selectedStyle: iPhoneOperationSystem? {
-        if let selectedVersion {
-            return entityData.first { $0.version == selectedVersion }
-        } else if !isSelected, let selectedData,
-                  let selectedIndex = cumulativeSalesRanges
-            .firstIndex(where: { $0.range.contains(selectedData) }) {
-            return entityData[selectedIndex]
+    private func updateSelectedStyle(selectedData: Double?, selectedType: ExpendType?, isSelected: Bool) {
+            if let selectedType {
+                selectedStyle = entityData.first { $0.expendType == selectedType }
+            } else if !isSelected, let selectedData, let selectedIndex = cumulativeSalesRanges.firstIndex(where: {  $0.range.contains(selectedData) }) {
+                selectedStyle = entityData[selectedIndex]
+            } else {
+                selectedStyle = nil
+            }
         }
-        return nil
+    
+    func getEntityData() -> [ExpendCount] {
+        return [ExpendCount(expendType: .date1, count: 15),
+                ExpendCount(expendType: .date2, count: 17),
+                ExpendCount(expendType: .date3, count: 23)]
     }
     
-    // MARK: - Percentage 반환
-    func calculatePercentage(for style: iPhoneOperationSystem) -> Int {
+    func calculatePercentage(for style: ExpendCount) -> Int {
         let total = entityData.reduce(into: 0) { $0 += $1.count }
         return Int(Double(style.count) / Double(total) * 100)
     }
     
-    // MARK: - 그래프 클릭시에 선택된 그래프 확인
     func handleSelection() {
         if let selectedData,
            let selectedIndex = cumulativeSalesRanges
             .firstIndex(where: { $0.range.contains(selectedData) }) {
-            let version = entityData[selectedIndex].version
-            if selectedVersion == version {
+            let expendType = entityData[selectedIndex].expendType
+            if selectedType == expendType {
                 // If tapping the same section, deselect it
-                selectedVersion = nil
+                selectedType = nil
                 isSelected = false
             } else {
                 // If tapping a different section, select it
-                selectedVersion = version
+                selectedType = expendType
                 isSelected = true
             }
         }
     }
 }
+
+// TODO: - 현재 월 기준으로 Entity를 받아와서 뿌려줘야 함. (지출 타입을 통해서)
+// TODO: - Entity를 받았을 때 지출, 수입 기준으로 나누고 화면이 나타날 땐 지출이 우선적으로 찍혀야 함.
+// TODO: - 지출 타입별로 카운트를 알아야 비율을 계산할 수 있음.
+// TODO: - 해당 Entity를 받으면 Cell을 통해서 또 뿌려줘야 함.
+
