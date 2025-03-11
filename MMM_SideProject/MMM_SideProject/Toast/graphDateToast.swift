70aaf68a1e0ded0a1afbd82dec5869d32ba0c4b5
kanghun1121 <kanghun1121@naver.com>
2025-03-11 13:00:49 +0900

    Feat: graphDateToast 생성

--- MMM_SideProject/MMM_SideProject/Toast/graphDateToast.swift
+++ MMM_SideProject/MMM_SideProject/Toast/graphDateToast.swift
@@ -8,12 +8,6 @@
 import UIKit
 
 class graphDateToast: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     let mainView : UIView = {
         let v = UIView()
@@ -25,10 +19,66 @@ class graphDateToast: UIViewController {
     }()
     
     lazy var headerStackView : UIStackView = {
-        let sv = UIStackView(arrangedSubviews: [])
+        let sv = UIStackView(arrangedSubviews: [headerLabel, headerButton])
         sv.translatesAutoresizingMaskIntoConstraints = false
         sv.axis = .horizontal
+        sv.distribution = .fill
+        sv.alignment = .fill
         return sv
     }()
     
+    let headerLabel : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .blackColor
+        label.font = UIFont(size: 14.0)
+        return label
+    }()
+    
+    let headerButton : UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "following"), for: .normal)
+        return button
+    }()
+    
+    let tableView : UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+    }
+    
+    func setLayout() {
+        view.addSubview(mainView)
+        mainView.addSubview(headerStackView)
+        mainView.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            mainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
+            mainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
+            mainView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            mainView.heightAnchor.constraint(equalToConstant: 400),
+            
+            headerStackView.leadingAnchor.constraint(equalTo: self.mainView.leadingAnchor),
+            headerStackView.trailingAnchor.constraint(equalTo: self.mainView.trailingAnchor),
+            headerStackView.topAnchor.constraint(equalTo: self.mainView.topAnchor),
+            headerStackView.heightAnchor.constraint(equalToConstant: 60),
+            
+            headerLabel.leadingAnchor.constraint(equalTo: self.headerStackView.leadingAnchor, constant: 15),
+            headerLabel.centerYAnchor.constraint(equalTo: self.headerStackView.centerYAnchor),
+            
+            headerButton.trailingAnchor.constraint(equalTo: self.headerStackView.trailingAnchor, constant: -15),
+            headerButton.centerYAnchor.constraint(equalTo: self.headerStackView.centerYAnchor),
+            
+            tableView.leadingAnchor.constraint(equalTo: self.mainView.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.mainView.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.mainView.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: self.headerStackView.bottomAnchor)
+        ])
+    }
 }
