be605ea8c02c415685bda39826edb115f71025bb
kanghun1121 <kanghun1121@naver.com>
2025-10-15 12:45:54 +0900

    Test Code 추가

---
+++ MMM_SideProject/MMM_SideProjectTests/MMM_SideProjectTests.swift
@@ -0,0 +1,27 @@
+//
+//  MMM_SideProjectTests.swift
+//  MMM_SideProjectTests
+//
+//  Created by 강대훈 on 10/15/25.
+//
+
+import XCTest
+
+final class MMM_SideProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testHello() {
+        XCTAssertTrue(true)
+    }
+    
+    func testHH() {
+        XCTAssertTrue(false)
+    }
+}
